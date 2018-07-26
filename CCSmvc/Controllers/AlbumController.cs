aa00df0614ce53608a6d6291cfd801f71360148f
Saroj Parida <SarojP@cogcis.onmicrosoft.com>
2018-07-26 15:07:53 +0000

    Added Team Album

---
+++ CCSmvc/Controllers/AlbumController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CCSmvc.Models;
+
+namespace CCSmvc.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Album
+        public ActionResult Index()
+        {
+            List<AlbumBlobDetails> blobs = new List<AlbumBlobDetails>();
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("teamalbum");
+
+            // Loop over items within the container and output the length and URI.
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {
+               
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    AlbumBlobDetails bDetails = new AlbumBlobDetails();
+                    var imgName= blob.Name.ToString();
+                    bDetails.Name = imgName;
+                    bDetails.Thumnail = imgName.Replace(".","_thumb.");
+                    blobs.Add(bDetails);
+                    Console.WriteLine("Block blob of length {0}: {1}", blob.Properties.Length, blob.Name);
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob pageBlob = (CloudPageBlob)item;
+                    Console.WriteLine("Page blob of length {0}: {1}", pageBlob.Properties.Length, pageBlob.Uri);
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory directory = (CloudBlobDirectory)item;
+                    Console.WriteLine("Directory: {0}", directory.Uri);
+                }
+            }
+            return View(blobs);
+        }
+    }
+}
---
+++ CCSmvc/Models/AlbumBlobDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CCSmvc.Models
+{
+    public class AlbumBlobDetails
+    {
+       
+        public string Name { get; set; }
+        public string Thumnail { get; set; }
+        public AlbumBlobDetails()
+        {
+            Url = "https://sarojwebappstorage.blob.core.windows.net/teamalbum/";
+            ThumbnailUrl = "https://sarojwebappstorage.blob.core.windows.net/teamalbumthumbnail/";
+        }
+    
+        public string Url { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
